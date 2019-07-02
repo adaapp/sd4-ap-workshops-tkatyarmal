f124065e162194e97ea21d4960c783fcaa40367d
Tejas Katyarmal <tkatyarmal@uk-c02tvca4htd6.uk.deloitte.com>
2019-07-02 12:53:55 +0100

    converted to ES6 syntax

--- example-encapsulation-workshop/classes/YearGroup.js
+++ example-encapsulation-workshop/classes/YearGroup.js
@@ -1,25 +1,21 @@
 class YearGroup {
     constructor(year, headOfYear) {
-        this.year = year,
-        this.headOfYear = headOfYear
+        
+        // This is how you create private attributes (ES6 syntax)
+        var _year = year,
+        var _headOfYear = headOfYear
+
+
+        // This is how I get the value from a private attribute (ES6 syntax)
+        // Because you can't do this._year anymore
+        this.getYear - function()   {
+            return _year
+        }
+        this.getHeadOfYear - function()   {
+            return _headOfYear
+        }
 
     }
 }
-
-function getYear()  {
-    return this.year
-}
-
-function getHeadOfYear()    {
-    return this.headOfYear
-}
-
-
-
-
-
-
-
-
 
 module.exports = YearGroup
